b218740b490ff8e3cbe4170706ec942c09731783
David Ke Hong <hongkedavid@gmail.com>
2015-01-28 18:50:13 -0500

    Create create_mirror.sh

---
+++ create_mirror.sh
@@ -0,0 +1,2 @@
+# mirroring traffic from or into port 1, 2 to por 3
+ovs-vsctl -- set bridge br0 mirrors=@m -- --id=@te-1/1/1 get Port te-1/1/1 -- --id=@te-1/1/2 get Port te-1/1/2 -- --id=@te-1/1/3 get Port te-1/1/3 -- --id=@m create Mirror name=managemirror select-dst-port=@te-1/1/1,@te-1/1/2 select-src-port=@te-1/1/1,@te-1/1/2 output-port=@te-1/1/3
