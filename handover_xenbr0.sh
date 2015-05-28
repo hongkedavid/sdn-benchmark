e035854a1424372eea22f5465a9730e7821bfe5d
David Ke Hong <hongkedavid@gmail.com>
2015-05-28 00:34:24 -0400

    Create handover_xenbr0.sh

---
+++ handover_xenbr0.sh
@@ -0,0 +1,4 @@
+route del -net 172.15.0.0 netmask 255.255.0.0 dev xenbr0
+route del -net 172.15.0.0 netmask 255.255.0.0 dev xenbr1
+route add -net 172.15.0.0 netmask 255.255.0.0 dev xenbr0
+ifconfig xenbr1 down
